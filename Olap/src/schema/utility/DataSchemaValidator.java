c5afcf015bcd45c96788f2ad10ad6ab201264e05
reda <reda@82152908-0416-11df-804c-772d9fe11005>
2010-01-07 22:45:44 +0000

    DataSchemaValidator ready

--- Olap/src/schema/utility/DataSchemaValidator.java
+++ Olap/src/schema/utility/DataSchemaValidator.java
@@ -16,10 +16,25 @@ import schema.Function;
  *
  */
 public class DataSchemaValidator {
+	private DataSchema schema;
+	private boolean validation;
 	
-	public static Boolean validateSchema(DataSchema schema){
+	public DataSchemaValidator(DataSchema schema){
+		this.schema = schema;
+		this.validation = validate();
+	}
+	
+	public boolean getValidation(){
+		return validation;
+	}
+	
+	private Boolean validate(){
+		return isAttributeNameDistinct()&&isFunctionNameDistinct()&&isExactlyOneRoot()&&isOnePathDistant();
+	}
+	
+	// The label of each attribute in a schema is distinct
+	private boolean isAttributeNameDistinct(){
 		boolean ok;
-		// The label of each attribute in a schema is distinct
 		Iterator<Attribute> attribute_iterator = schema.getAttributeIterator();
 		Set<String> attribute_set = new HashSet<String>();
 		while(attribute_iterator.hasNext()){
@@ -28,8 +43,12 @@ public class DataSchemaValidator {
 				return false;
 			}
 		}
-		
-		// The label of each function in a schema is distinct 
+		return true;
+	}
+	
+	// The label of each function in a schema is distinct 
+	private boolean isFunctionNameDistinct(){
+		boolean ok;
 		Iterator<Function> function_iterator = schema.getFunctionIterator();
 		Set<String> function_set = new HashSet<String>();
 		while(function_iterator.hasNext()){
@@ -38,14 +57,64 @@ public class DataSchemaValidator {
 				return false;
 			}
 		}
-		
-		// A data schema has exactly one root
-		
-		
-		// Each node in a data schema has at least one path from the root of the schema 
-		
-		
 		return true;
 	}
 	
+	// A data schema has exactly one root
+	private boolean isExactlyOneRoot(){
+		int i=0;
+		Iterator<Attribute> attribute_iterator = schema.getAttributeIterator();
+		Attribute att;
+		while(attribute_iterator.hasNext()){
+			att = attribute_iterator.next();
+			if(schema.getFunctionByDomain(att)!= null && schema.getFunctionByRange(att) == null){
+				i++;
+			}
+		}
+		return i==1;
+	}
+	
+	// Each node in a data schema has at least one path from the root of the schema 
+	private boolean isOnePathDistant(){
+		// First, we find the root
+		boolean root_found = false;
+		Iterator<Attribute> attribute_iterator = schema.getAttributeIterator();
+		Attribute att;
+		Attribute root = null;
+		
+		while(attribute_iterator.hasNext()&&!root_found){
+			att = attribute_iterator.next();
+			if(schema.getFunctionByDomain(att)!= null && schema.getFunctionByRange(att) == null){
+				root=att;
+				root_found = true;
+			}
+		}
+		
+		// Now we test that each node is linked to the root
+		boolean root_reached;
+		attribute_iterator = schema.getAttributeIterator();
+		Function func;
+		
+		while(attribute_iterator.hasNext()){
+			att = attribute_iterator.next();
+			if(!att.equals(root)){
+				root_reached = false;
+				
+				while(!root_reached){
+					func = schema.getFunctionByRange(att);
+					if(func == null){
+						return false;
+					}
+					else if(func.getDomain().equals(root)){
+						root_reached = true;
+					}
+					else{
+						att = func.getDomain();
+					}
+				}
+			}
+		}
+		
+		return true;
+	}
 }
