42cd802e921ea7e9749c030bd2fbff56b8ad92f0
julien.coutand <julien.coutand@82152908-0416-11df-804c-772d9fe11005>
2010-01-18 20:19:36 +0000

    Correction about immutable object

--- Olap/src/query/implementation/FunctionReference.java
+++ Olap/src/query/implementation/FunctionReference.java
@@ -11,55 +11,71 @@ import schema.Attribute;
 import schema.Function;
 
 /**
- *	FunctionReference is a PathExpression that only referred to a SchemaFunction
- *	For example the simple path "g" is a FunctionReference.
+ * FunctionReference is a PathExpression that only referred to a SchemaFunction
+ * For example the simple path "g" is a FunctionReference. Object of this class
+ * are immutable
  */
-public class FunctionReference implements PathExpression{
+public class FunctionReference implements PathExpression {
 
 	/**
-	 * This is the {@link #Function} this class referred to
+	 * This is the {@link #Function} this class referred to Function object are
+	 * immutable
 	 */
 	private Function referredFunction;
 	
 	/**
+	 * Reference to range attribute iterator
+	 */
+	private Iterator<Attribute> rangeIterator;
+	
+	/**
+	 * Reference to domain attribute iterator
+	 */
+	private Iterator<Attribute> domainIterator;
+
+	/**
 	 * Construct FunctionReference using referredFunction as parameter
-	 * @param referredFunction - The function to refer to
+	 * 
+	 * @param referredFunction
+	 *            - The function to refer to (immutable)
 	 */
 	public FunctionReference(Function referredFunction) {
 		super();
 		this.referredFunction = referredFunction;
+		
+		//save iterators now, because referredFunction is immutable
+		//multiple acces to these members will be more efficient
+		ArrayList<Attribute> rangeList = new ArrayList<Attribute>();
+		rangeList.add(referredFunction.getRange());
+		rangeIterator = rangeList.iterator();
+		
+		ArrayList<Attribute> domainList = new ArrayList<Attribute>();
+		domainList.add(referredFunction.getDomain());
+		domainIterator = domainList.iterator();
 	}
 
 	/**
 	 * @return the referredFunction this object referred to
 	 */
-	Function getRefferedFunction(){
+	Function getRefferedFunction() {
 		return this.referredFunction;
 	}
-	
+
 	/**
-	 * Be carefull, referredFunction can be modify by other object and its domain can change.
 	 * @return the attribute from referredFunction's domain.
 	 * @see Function#getDomain();
 	 */
 	@Override
 	public Iterator<Attribute> getDomain() {
-		ArrayList<Attribute> returnList = new ArrayList<Attribute>();
-		returnList.add(referredFunction.getDomain());
-		
-		return returnList.iterator();
+		return rangeIterator;
 	}
 
 	/**
-	 * Be carefull, referredFunction can be modify by other object and its domain can change.
 	 * @return the attribute from referredFunction's domain.
 	 * @see Function#getRange();
 	 */
 	@Override
 	public Iterator<Attribute> getRange() {
-		ArrayList<Attribute> returnList = new ArrayList<Attribute>();
-		returnList.add(referredFunction.getRange());
-		
-		return returnList.iterator();
+		return domainIterator;
 	}
 }
