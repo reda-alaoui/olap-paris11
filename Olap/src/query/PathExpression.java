1b21428ea73d916a1e2f442b88fd6f05f5a3fc36
julien.coutand <julien.coutand@82152908-0416-11df-804c-772d9fe11005>
2010-01-18 18:26:39 +0000

    Quick commit about Tsuyoshi SUGIBUCHI mail of 18/01. Implement parts of FunctionReference and PathExpression.

--- Olap/src/query/PathExpression.java
+++ Olap/src/query/PathExpression.java
@@ -3,13 +3,24 @@
  */
 package query;
 
+import java.util.List;
+
+import schema.Attribute;
+
 /**
- * Represent an Olap path
- *
  * The path expression represent an Olap path.
  * It is a tree, its leaf are FunctionReference and its root is a FunctionReference or an Operator
  */
 public interface PathExpression {	
 	
+	/**
+	 * @return the domain as Attributes List
+	 */
+	List<Attribute> getDomain();
 	
+	/**
+	 * 
+	 * @return the range as Attributes List
+	 */
+	List<Attribute> getRange();
 }
--- Olap/src/query/implementation/FunctionReference.java
+++ Olap/src/query/implementation/FunctionReference.java
@@ -3,12 +3,49 @@
  */
 package query.implementation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import query.PathExpression;
+import schema.Attribute;
+import schema.Function;
 
 /**
- * @author Reda
- *
+ *	FunctionReference is a PathExpression that only referred to a SchemaFunction
+ *	For example the simple path "g" is a FunctionReference.
  */
 public class FunctionReference implements PathExpression{
 
+	/**
+	 * This is the {@link #Function} this class referred to
+	 */
+	private Function referredFunction;
+	
+	/**
+	 * Construct FunctionReference using referredFunction as parameter
+	 * @param referredFunction - The function to refer to
+	 */
+	public FunctionReference(Function referredFunction) {
+		super();
+		this.referredFunction = referredFunction;
+	}
+
+	Function getRefferedFunction(){
+		return this.referredFunction;
+	}
+	
+
+	@Override
+	public List<Attribute> getDomain() {
+		ArrayList<Attribute> returnList = new ArrayList<Attribute>();
+		returnList.add(referredFunction.getDomain());
+		return returnList;
+	}
+
+	@Override
+	public List<Attribute> getRange() {
+		ArrayList<Attribute> returnList = new ArrayList<Attribute>();
+		returnList.add(referredFunction.getRange());
+		return returnList;
+	}
 }
