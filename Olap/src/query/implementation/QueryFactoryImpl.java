ef43bea983c813fc1288e6ff8fe24f6740d76c33
alaoui.rda <alaoui.rda@82152908-0416-11df-804c-772d9fe11005>
2010-01-24 15:24:17 +0000

    Creating QueryFactoryImpl

---
+++ Olap/src/query/implementation/QueryFactoryImpl.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package query.implementation;
+
+import java.util.List;
+
+import query.Composition;
+import query.FunctionReference;
+import query.OlapQuery;
+import query.Pairing;
+import query.PathExpression;
+import query.Projection;
+import query.QueryFactory;
+import schema.Attribute;
+import schema.DataSchema;
+
+/**
+ * @author Reda
+ *
+ */
+public class QueryFactoryImpl implements QueryFactory {
+	
+	private DataSchema schema;
+	
+	public QueryFactoryImpl(DataSchema schema){
+		this.schema = schema;
+	}
+
+	/* (non-Javadoc)
+	 * @see query.QueryFactory#composition(query.PathExpression, query.PathExpression)
+	 */
+	@Override
+	public Composition composition(PathExpression p1, PathExpression p2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see query.QueryFactory#function(java.lang.String)
+	 */
+	@Override
+	public FunctionReference function(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see query.QueryFactory#olapQuery(query.PathExpression, query.PathExpression, query.QueryFactory.AggregationFunction)
+	 */
+	@Override
+	public OlapQuery olapQuery(PathExpression c, PathExpression m,
+			AggregationFunction op) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see query.QueryFactory#pairing(query.PathExpression, query.PathExpression)
+	 */
+	@Override
+	public Pairing pairing(PathExpression p1, PathExpression p2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see query.QueryFactory#projection(query.PathExpression, java.util.List)
+	 */
+	@Override
+	public Projection projection(PathExpression p, List<Attribute> selectList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Olap/src/query/test/QueryFactoryImplTest.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package query.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Reda
+ *
+ */
+public class QueryFactoryImplTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link query.implementation.QueryFactoryImpl#QueryFactoryImpl(schema.DataSchema)}.
+	 */
+	@Test
+	public void testQueryFactoryImpl() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link query.implementation.QueryFactoryImpl#composition(query.PathExpression, query.PathExpression)}.
+	 */
+	@Test
+	public void testComposition() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link query.implementation.QueryFactoryImpl#function(java.lang.String)}.
+	 */
+	@Test
+	public void testFunction() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link query.implementation.QueryFactoryImpl#olapQuery(query.PathExpression, query.PathExpression, query.QueryFactory.AggregationFunction)}.
+	 */
+	@Test
+	public void testOlapQuery() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link query.implementation.QueryFactoryImpl#pairing(query.PathExpression, query.PathExpression)}.
+	 */
+	@Test
+	public void testPairing() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link query.implementation.QueryFactoryImpl#projection(query.PathExpression, java.util.List)}.
+	 */
+	@Test
+	public void testProjection() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
