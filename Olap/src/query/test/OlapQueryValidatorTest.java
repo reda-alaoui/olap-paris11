640373ff72cb69364cd0ce6bf6e6545971c5d147
sylvestre.koumar <sylvestre.koumar@82152908-0416-11df-804c-772d9fe11005>
2010-01-31 21:28:13 +0000

    warnings removed

--- Olap/src/query/test/OlapQueryValidatorTest.java
+++ Olap/src/query/test/OlapQueryValidatorTest.java
@@ -1,6 +1,6 @@
 package query.test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
@@ -12,7 +12,6 @@ import query.QueryFactory.AggregationFunction;
 import query.implementation.OlapQueryImpl;
 import query.implementation.QueryFactoryImpl;
 import query.utility.OlapQueryValidator;
-import query.utility.PathExpressionValidationException;
 import schema.Attribute;
 import schema.DataSchema;
 import schema.Function;
@@ -27,12 +26,14 @@ import schema.implementation.FunctionImpl;
 public class OlapQueryValidatorTest {
 
 	private OlapQuery query;
-	
+
 	private DataSchema schema;
-	
+
 	private boolean validation;
-	
-	
+
+	private OlapQueryValidator oqv;
+
+
 	@Before
 	public void setUp() throws Exception {
 
@@ -41,50 +42,51 @@ public class OlapQueryValidatorTest {
 		ArrayList<Function> func_list = new ArrayList<Function>();
 		//Defining attributes
 		att_list.add(new AttributeImpl("O", DataType.ID));
-		att_list.add(new AttributeImpl("Quantity",DataType.INTEGER));
 		att_list.add(new AttributeImpl("Product", DataType.STRING));
+		att_list.add(new AttributeImpl("Quantity",DataType.INTEGER));
 		att_list.add(new AttributeImpl("Supplier", DataType.STRING));
 		att_list.add(new AttributeImpl("Category", DataType.STRING));
-		
+
 		//Defining functions
-		func_list.add(new FunctionImpl("q", att_list.get(0), att_list.get(1)));
-		func_list.add(new FunctionImpl("f", att_list.get(0), att_list.get(2)));
-		func_list.add(new FunctionImpl("f1", att_list.get(2), att_list.get(3)));
-		func_list.add(new FunctionImpl("f2", att_list.get(2), att_list.get(4)));
-		
+		func_list.add(new FunctionImpl("h", att_list.get(0), att_list.get(1)));
+		func_list.add(new FunctionImpl("q", att_list.get(0), att_list.get(2)));
+		func_list.add(new FunctionImpl("h2", att_list.get(1), att_list.get(3)));
+		func_list.add(new FunctionImpl("h1", att_list.get(1), att_list.get(4)));
+
 		schema = new DataSchemaImpl(name,att_list,func_list);
-		
+
 		QueryFactoryImpl q = new QueryFactoryImpl(schema);
-		
+
 		ArrayList<Attribute> attList = new ArrayList<Attribute>();
 		attList.add(new AttributeImpl("Quantity",DataType.INTEGER));
 		attList.add(new AttributeImpl("Product", DataType.STRING));
 		//selected attributes
 		ArrayList<Attribute> attList2 = new ArrayList<Attribute>();
 		attList2.add(new AttributeImpl("Product", DataType.STRING));
-		
-		
+
+
 		query = OlapQueryImpl.createOlapQuery(q.composition(	
-					q.projection(attList, attList2),
-					q.pairing(
-							q.function("q"),
-							q.function("f")
-					)), q.function("q"), AggregationFunction.COUNT);
-		
+				q.projection(attList, attList2),
+				q.pairing(
+						q.function("q"),
+						q.function("h")
+				)), q.function("q"), AggregationFunction.SUM);
+
 		validation = false;
-		
+
 	}
 
 
 	@Test
 	public void testOlapQueryValidator() {
-		OlapQueryValidator oqv = new OlapQueryValidator(query, schema);
+		oqv = new OlapQueryValidator(query, schema);
 	}
 
 	@Test
 	public void testGetValidation() {
-		OlapQueryValidator oqv = new OlapQueryValidator(query, schema);
+		oqv = new OlapQueryValidator(query, schema);
 		validation = oqv.getValidation();
+		assertTrue(validation);
 	}
 
 }
